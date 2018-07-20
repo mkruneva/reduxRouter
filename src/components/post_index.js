956ad48489882f44a58080542d364d307a760df2
Marta Kruneva <marta.kruneva@gmail.com>
2018-07-20 20:47:38 +0100

    initial commit

---
+++ src/components/post_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostIndex extends React.Component {
+  render() {
+    return(
+      <div>
+        Post Index
+      </div>
+    )
+  }
+}
+
+export default PostIndex;
