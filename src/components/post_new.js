4d0e12eb0dd34e5bff8872845acf88e796500dfb
Marta Kruneva <marta.kruneva@gmail.com>
2018-07-22 18:36:38 +0100

    redux-post form

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form'; // reduxForm - similar to connect , aloows component to talk directly to redux Store
 
 class PostNew extends Component {
+  renderField(field) {
+    console.log(field);
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input type={field.type} className="form-control"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
         <div className="text-xs-right">
           <NavLink className="btn btn-primary" to='/'>Back to Posts</NavLink>
         </div>
-        Post New
+        <form action="">
+        <Field 
+          label="Title"
+          type="text"
+          name="title"
+          component={this.renderField}
+        />
+        <Field 
+          label="Tags"
+          type="password"
+          name="tags"
+          component={this.renderField}
+        />
+        </form>
       </div>
     )
   }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'PostNewForm'
+})(PostNew);
