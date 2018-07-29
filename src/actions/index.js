38d7f4f705e7920f19b22e8dfaf81057f9743481
mkruneva <marta.kruneva@gmail.com>
2018-07-29 11:24:22 +0100

    fetchPost action creater, reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,10 @@ const API_KEY = '?key=135798642';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_API}/posts${API_KEY}`);
-
 
   return {
     type: FETCH_POSTS,
@@ -22,6 +22,15 @@ export function createPost(values, callback) {
 
   return {
     type: CREATE_POST,
+    payload: request
+  }
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_API}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: request
   }
 }
